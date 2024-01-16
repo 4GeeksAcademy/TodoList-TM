9475c6a769e44017924f2dea47debaaeccb85a0c
tomasmj503 <86091325+tomasmj503@users.noreply.github.com>
2024-01-16 17:11:46 +0000

    Todo list

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -5,49 +5,50 @@ import "/workspaces/TodoList-TM/src/styles/home.css"
 const Home = () => {
     const [input, setInput] = useState("")
     const [tareas, setTareas] = useState([])
- 
+
 
     function manejoEnvio(e) {
         e.preventDefault()
-        const tareasActualizadas = [input, ...tareas]
-        setTareas(tareasActualizadas)
-        setInput("")
+        if (input.trim() !== "") {
+            const tareasActualizadas = [input, ...tareas]
+            setTareas(tareasActualizadas)
+            setInput("")
+        }
     }
 
     function borrar(id) {
         let nuevo = []
         nuevo = tareas.filter((item, index) => {
-            if(index != id) {
+            if (index != id) {
                 return item
             }
-        } )
+        })
         setTareas(nuevo)
     }
 
     return (
         <div className="tarea-contenedor">
             <form className="tarea-formulario"
-            onSubmit={manejoEnvio}>
+                onSubmit={manejoEnvio}>
                 <input
-                className="tarea-input"
-                type="text" 
-                placeholder="Escribe una tarea"
-                value={input}
-                onChange={(e) => setInput(e.target.value)}
+                    className="tarea-input"
+                    type="text"
+                    placeholder="Escribe una tarea"
+                    value={input}
+                    onChange={(e) => setInput(e.target.value)}
                 />
             </form>
             <ul className="list-unstyled ">
-                {tareas.map((elemento, id) => ( 
-                    <li key={id}> 
-                
-                {id} - {elemento} 
-                <button onClick={() => borrar(id)}>x</button>
-                </li>
+                {tareas.map((elemento, id) => (
+                    <li key={id} style={{ display: 'flex', justifyContent: 'space-between' }}>
+                        {elemento}
+                        <button className={"trash"}onClick={() => borrar(id)}><i class="fa-solid fa-trash"></i></button>
+                    </li>
                 ))}
             </ul>
 
             <p>Tareas pendientes: {tareas.length} </p>
-        </div>       
+        </div>
     )
 }
 
